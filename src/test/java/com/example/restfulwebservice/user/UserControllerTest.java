8d4d0ced7c7b055763228524524a5173c0ee2fd9
hc84.shin <dzzwe@naver.com>
2021-07-23 16:45:18 +0900

    make 테스트코드

---
+++ src/test/java/com/example/restfulwebservice/user/UserControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.restfulwebservice.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserControllerTest {
+
+    @Autowired
+    private UserDaoService service;
+
+    @Test
+    void retrieveAllUsers() {
+        List<User> list = service.findAll();
+        assertEquals(list.size(),3);
+    }
+
+    @Test
+    void retrieveUser() {
+    }
+
+    @Test
+    void createUser() {
+        User user = new User(1, "test1", new Date());
+        User user2 = service.save(user);
+        assertEquals(user.getId(),user2.getId());
+    }
+}
