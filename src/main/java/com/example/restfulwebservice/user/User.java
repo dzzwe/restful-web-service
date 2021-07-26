4c65a257790ce9dabd6a7992cdef82edf8f78fe5
dzzwe <dzzwe@naver.com>
2021-07-27 00:21:54 +0900

    vailidation 추가

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,12 +3,16 @@ package com.example.restfulwebservice.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+
 import java.util.Date;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
+
+    @Size(min=2)
     private String name;
     private Date joinData;
 }
