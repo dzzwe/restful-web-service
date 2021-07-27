498f3f5fb9315730405c5d66ce025b102d91ab8f
dzzwe <dzzwe@naver.com>
2021-07-27 23:46:29 +0900

    다국어 처리, validation 처리

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 
 import java.util.Date;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -13,7 +14,7 @@ import javax.validation.constraints.Size;
 public class User {
     private Integer id;
 
-    @Size(min=2)
+    @Size(min=2, message = "Name은 2글자 이상 입력해주세요.")
     private String name;
 
     @Past
